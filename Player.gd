6797d20a627a8657148907ffb3792c676c2b0e7c
Vignesh S <vigneshs6993@gmail.com>
2022-07-23 22:01:02 +0530

    Polished platformer controller

--- Player.gd
+++ Player.gd
@@ -1,17 +1,45 @@
 extends KinematicBody2D
 
+export var jump_force := 190
+export var jump_release_force := 70
+export var max_speed := 100
+export var acceleration := 20
+export var friction := 20
+export var gravity := 8
+export var additional_fall_gravity := 8
+
 var velocity := Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	velocity.y += 8
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -50
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x = 50
-	else:
-		velocity.x = 0
+	apply_gravity()
+	var input_axis = Vector2.ZERO
+	input_axis.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
-	if Input.is_action_just_pressed("ui_up"):
-		velocity.y = -150
-	velocity = move_and_slide(velocity)
+	if input_axis.x == 0:
+		apply_friction()
+	else:
+		apply_acceleration(input_axis.x)
+	
+	if is_on_floor():
+		if Input.is_action_pressed("ui_up"):
+			velocity.y = -jump_force
+	else:
+		if Input.is_action_just_released("ui_up") and velocity.y < -jump_release_force:
+			velocity.y = -jump_release_force
+		if velocity.y > 0: # Celeste like fast fall
+			velocity.y += additional_fall_gravity
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	pass
+
+func apply_gravity():
+	velocity.y += gravity
+	pass
+
+func apply_friction():
+	velocity.x = move_toward(velocity.x, 0, friction)
+	pass
+
+func apply_acceleration(direction):
+	velocity.x = move_toward(velocity.x, max_speed * direction, acceleration)
 	pass
